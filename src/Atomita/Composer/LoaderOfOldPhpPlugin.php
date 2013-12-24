5e93be9bad50cb024b76c7e4fff8f026ed8d9ed2
@ <sleeping.cait.sith+gh@gmail.com>
2013-12-24 21:03:44 +0900

    Create LoaderOfOldPhpPlugin.php

---
+++ src/Atomita/Composer/LoaderOfOldPhpPlugin.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace \Atomita\Composer;
+
+use Composer\Composer;
+use Composer\EventDispatcher\EventSubscriberInterface;
+use Composer\IO\IOInterface;
+use Composer\Plugin\PluginInterface;
+use Composer\Script\ScriptEvents;
+use Composer\Script\Event as ScriptEvent;
+use Composer\Util\Filesystem;
+
+class LoaderOfOldPhpPlugin implements PluginInterface, EventSubscriberInterface {
+	
+	protected $composer;
+	protected $io;
+ 
+    public function activate(Composer $composer, IOInterface $io) {
+		$this->composer = $composer;
+		$this->io = $io;
+	}
+
+	public static function getSubscribedEvents() {
+		return array(
+			ScriptEvents::POST_AUTOLOAD_DUMP => array(
+				array('onPostAutoloadDump', 0)
+			),
+		);
+	}
+	
+	public function onPostAutoloadDump(ScriptEvent $event) {
+		$config = $event->getComposer()->getConfig();
+		
+		$filesystem = new Filesystem();
+		$vendorPath = $filesystem->normalizePath(realpath($config->get('vendor-dir')));
+		$targetDir  = $vendorPath . '/composer';
+		
+		// @todo
+	}
+	
+}
